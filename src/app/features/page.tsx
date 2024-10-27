dbe83744f29c1a4079d3e9e4235d2a609a9df3c4
Meraj <haquedot@gmail.com>
2024-10-27 23:05:11 +0530

    added features page

---
+++ src/app/features/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import Features from '@/components/Features';
+import React from 'react';
+
+export default function Feature() {
+    return (
+        <div className="sm:w-10/12 mx-auto flex items-center justify-center py-20 px-4">
+            <Features />
+        </div>
+    )
+}
