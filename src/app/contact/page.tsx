380620115a4176ac58d1ca23c1411f6441683919
Meraj <haquedot@gmail.com>
2024-10-28 23:17:32 +0530

    created contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+// contact page
+'use client';
+
+import Contact from '@/components/Contact';
+import React from 'react';
+
+export default function ContactPage() {
+    return (
+        <>
+            <div className="sm:w-10/12 max-w-lg mx-auto flex items-center justify-center py-20 px-4">
+                <Contact />
+            </div>
+        </>
+    );
+};
---
+++ src/components/Contact.tsx
@@ -0,0 +1,21 @@
+// Contact us component with form 
+'use client';
+
+import React from 'react';
+
+export default function Contact() {
+    return (
+        <div className="w-full shadow-md border rounded-md text-neutral-900 bg-white p-6">
+            <h3 className="text-black text-xl font-semibold mb-4">Contact Us</h3>
+            <form className="flex flex-col space-y-4">
+                <label htmlFor="name" className="text-black">Name</label>
+                <input type="text" id="name" name="name" className="py-2 px-4 rounded-md bg-neutral-100 text-black"  placeholder='Enter your name'/>
+                <label htmlFor="email" className="text-black">Email</label>
+                <input type="email" id="email" name="email" className="py-2 px-4 rounded-md bg-neutral-100 text-black" placeholder='Enter your email' />
+                <label htmlFor="message" className="text-black">Message</label>
+                <textarea id="message" name="message" className="py-2 px-4 rounded-md bg-neutral-100 text-black" rows={5} placeholder='Type your message...'></textarea>
+                <button type="submit" className="bg-primary-500 text-black py-2 rounded-md">Send</button>
+            </form>
+        </div>
+    );
+};
