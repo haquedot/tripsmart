a16e77f8347f5e20e45bfc8a4e76fdd26e0754dc
Meraj <haquedot@gmail.com>
2024-11-08 22:59:07 +0530

    added logout button to sidebar of dashboard

--- src/components/DashboardSidebar.tsx
+++ src/components/DashboardSidebar.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { MdDashboard } from "react-icons/md";
-import { IoIosNotifications, IoMdSettings } from "react-icons/io";
+import { IoIosNotifications, IoMdLogOut, IoMdSettings } from "react-icons/io";
 import { useState } from "react";
 import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
 import { FaTicket } from "react-icons/fa6";
 import Calendar from "@/components/Calendar";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 type View = "dashboard" | "myTrips" | "notifications" | "settings";
 
 const DashboardSidebar = () => {
   const pathName = usePathname();
   const [open, setOpen] = useState(false);
+  const router = useRouter();
 
   const links = [
     {
@@ -41,17 +43,19 @@ const DashboardSidebar = () => {
     },
   ];
 
-  // const handleLogout = () => {
-  //   document.cookie = `token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
-  //   toast.success("Logout successful!");
-  //   router.push("/login");
-  // };
+  const handleLogout = () => {
+    if (typeof window !== "undefined") {
+      document.cookie = `token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+      toast.success('Logout successful!');
+      router.push('/login');
+    }
+  };
 
   return (
     <>
       <Sidebar open={open} setOpen={setOpen} animate={false}>
         <SidebarBody className="justify-between gap-4">
-          <div className="flex flex-col flex-1 px-4 py-4 rounded-md bg-indigo-50 dark:bg-neutral-800  overflow-y-auto overflow-x-hidden">
+          <div className="flex flex-col flex-1 px-4 py-4 rounded-md bg-indigo-50 dark:bg-neutral-800 overflow-y-auto overflow-x-hidden">
             {/* <Logo LogoHeight={45} LogoWidth={45}/> */}
 
             <div className="flex flex-col gap-2">
@@ -60,20 +64,22 @@ const DashboardSidebar = () => {
                   key={idx}
                   link={link}
                   active={pathName == link.href}
-                 
+
                 />
               ))}
             </div>
+
+            <button
+              onClick={handleLogout}
+              className="w-full flex flex-center text-red-500 hover:text-red-600 mt-4 bg-red-100 hover:bg-red-200 px-4 py-2 rounded-md"
+            >
+              <IoMdLogOut className="h-5 w-5 flex-shrink-0" />
+              <span className="ml-2">Logout</span>
+            </button>
           </div>
+
           <div>
             <Calendar />
-            {/* <button
-                  onClick={handleLogout}
-                  className="flex flex-center text-red-500 hover:text-red-600"
-                >
-                  <IoMdLogOut className="h-5 w-5 flex-shrink-0" />
-                  <span className="ml-2">Logout</span>
-                </button> */}
           </div>
         </SidebarBody>
       </Sidebar>
